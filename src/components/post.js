17b097714147621a254de47356eb8f67a9f83eab
ivyfu77 <ivy@hmsnz.com>
2017-09-28 12:27:38 +1300

    Add author/category for post-item and style them

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { timeToFormatDate } from '../utils/lib';
 import { getPostComments } from '../utils/api';
+import { Link } from "react-router-dom";
 
 class Post extends Component {
   state = {
@@ -20,16 +21,26 @@ class Post extends Component {
     const { comments } = this.state;
     return (
       <div className="post-item">
-        <div className="post-item-first-line">
-          <p>{timeToFormatDate(post.timestamp)}</p>
-          <div className="vote-score">
-            <i className="fa fa-minus" aria-hidden="true"></i>
-            <span>{post.voteScore}</span>
-            <i className="fa fa-plus" aria-hidden="true"></i>
+        <div className="post-item-content">
+          <div className="post-item-first-line">
+            <p>{timeToFormatDate(post.timestamp)}</p>
+            <div className="vote-score">
+              <i className="fa fa-minus" aria-hidden="true"></i>
+              <span>{post.voteScore}</span>
+              <i className="fa fa-plus" aria-hidden="true"></i>
+            </div>
+            <div className="comment-number">{comments.length} comments</div>
           </div>
-          <div className="comment-number">{comments.length} comments</div>
+          <h4>{ post.title }</h4>
+          <div className="post-item-bottom">
+            <span className="post-author">By {post.author}</span>
+            <Link to={"/"+post.category}><span className="post-category">{post.category.toUpperCase()}</span></Link>
+          </div>
         </div>
-        <h4>{ post.title }</h4>
+        <div className="post-item-action">
+          <i className="fa fa-pencil-square" aria-hidden="true"></i>
+          <i className="fa fa-minus-square" aria-hidden="true"></i>
+        </div>
       </div>
     );
   }
